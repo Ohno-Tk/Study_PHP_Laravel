0a8be32bb8dbf05e4efa15140390e5f0f42584d3
takuya-ohno <ohno.takuya.1502@gmail.com>
2020-11-28 11:07:56 +0900

    継承レイアウトの作成

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,35 +1,17 @@
-<html>
-<head>
-<title>Hello/index</title>
-<style>
-    body {font-size:16pt; color:#999;}
-    h1 {foont-size:100pt; text-align:right; color:#eee;
-        margin:-40px 0px -50px 0px; letter-spacing:-4px; }
-</style>
-</head>
-<body>
-    <h1>Blade/Index</h1>
-    <p>&#064;forディレクティブの例</p>
-    <ol>
-      @for ($i = 1;$i < 100;$i++)
-      @if ($i %2 == 1)
-        @continue
-      @elseif ($i <= 10)
-        <li>No,{{$i}}
-      @else
-        @break
-      @endif
-      @endfor
-    </ol>
+@extends('layouts.helloapp')
 
-    @foreach ($data as $item)
-    @if($loop->first)
-      <p>※データ一覧</p><ul>
-    @endif
-    <li>No,{{$loop->iteration}}.{{$item}}</li>
-    @if($loop->last)
-    </ul><p>-----ここまで</p>
-    @endif
-    @endforeach
-</body>
-</html>
+@section('title','Index')
+
+@section('menubar')
+  @parent
+  インデックスページ
+@endsection
+
+@section('content')
+  <p>ここが本文のコンテンツです。</p>
+  <p>必要なだけ記述できます。</p>
+@endsection
+
+@section('footer')
+copyright 2020 ohno.
+@endsection
