0d361744176de656f0ff7c11dec7aacf69731bf8
takuya-ohno <ohno.takuya.1502@gmail.com>
2020-11-28 14:37:08 +0900

    DBのinsertの使用例

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -15,12 +15,24 @@ class HelloController extends Controller
     }
 
     public function post(Request $request){
-        $validate_rule = [
-            'name'=>'required',
-            'mail'=>'email',
-            'age'=>'numeric|between:0,150',
+
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items'=>$items]);
+    }
+
+    public function add(Request $request){
+        return view('hello.add');
+    }
+
+    public function create(Request $request){
+        $param = [
+            'name'=>$request->name,
+            'mail'=>$request->mail,
+            'age'=>$request->age,
         ];
-        $this->validate($request, $validate_rule);
-        return view('hello.index', ['msg'=>'正しく入力されました!']);
+
+        DB::insert('insert into people(name, mail,age) values(:name, :mail, :age)', $param);
+
+        return redirect('/hello');
     }
 }
