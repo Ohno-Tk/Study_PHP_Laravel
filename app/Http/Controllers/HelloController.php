55ccf817dcf420cc9ec7ee194a278c591116677f
takuya-ohno <ohno.takuya.1502@gmail.com>
2020-11-28 09:32:59 +0900

    値をテンプレートに渡す

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,28 +7,10 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request, Response $response){
-        $html = <<<EOF
+    public function index(){
 
-        <html>
-        <head>
-        <title>Hello/index</title>
-        <style>
-            body {font-size:16pt; color:#999;}
-            h1 {foont-size:100pt; text-align:right; color:#eee;
-                margin:-40px 0px -50px 0px; }
-        </style>
-        </head>
-        <body>
-            <h1>hello</h1>
-            <h3>Request</h3>
-            <pre>{$request}</pre>
-            <h3>Response</h3>
-            <pre>{$response}</pre>
-        </body>
-        </html>
-        EOF;
-                $response->setContent($html);
-                return $response;
+        $date = ['msg'=>'これはコントローラから渡されたメッセージです。'];
+
+        return view('hello.index', $date);
     }
 }
